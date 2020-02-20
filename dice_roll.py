240bd74fb381365330a4a46e96f3ab2cc3207df0
Simeon Raykov <makenr3@gmail.com>
2020-02-20 11:32:19 +0200

    Add files via upload

---
+++ dice_roll.py
@@ -0,0 +1,5 @@
+import random
+def dice_roll():
+    return random.randint(1,6)
+
+print(dice_roll())
---
+++ quick_sort.py
@@ -0,0 +1,13 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quicksort(left) + middle + quicksort(right)
+
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print(quicksort(test))
