d02d7cf8d96768e0bd6789ed7b2178d28cd0543d
Simeon Raykov <makenr3@gmail.com>
2021-12-21 14:14:07 +0200

    google coding challenge

---
+++ addOneToArray.js
@@ -0,0 +1,26 @@
+// Google coding challenge
+// Add one in the array and return a new array
+
+function sumArrayNumbers(arr) {
+
+    let resultArr = [...arr];
+
+    for (let i = resultArr.length - 1; i >= 0; i -= 1) {
+        if (resultArr[i] !== 9) {
+            resultArr[i] += 1;
+            break;
+        } else {
+            resultArr[i] = 0;
+            if (i === 0) {
+                resultArr.unshift(1);
+            }
+        }
+    }
+
+    return resultArr;
+}
+
+
+sumArrayNumbers([9, 9, 9, 8, 9]);
+sumArrayNumbers([2, 3, 4, 9, 9]);
+sumArrayNumbers([9]);
